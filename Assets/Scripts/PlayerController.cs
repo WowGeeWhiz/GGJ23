d531b539478fd2c8b3993eebb86664810b21bbf8
Ethan Johnson <ethan.d.r.johnson@gmail.com>
2023-02-03 23:56:16 -0500

    Redid player movement
    Players now collide with other colliders

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,83 +8,30 @@ public class PlayerController : MonoBehaviour
 {
     //player variables
     public int speed; //player movement speed
-    public Rigidbody2D rb; //player rigidbody
-    public Collider2D col; //player hitbox
-    private bool movedUp, movedDown, movedLeft, movedRight; //directional bools
-    public Collider2D leftAttack, rightAttack, upAttack, downAttack;
-    internal bool attacking = false;
-    public float attackHideDelay;
-    internal float lastAttackTime;
+    public Collider2D leftAttack, rightAttack, upAttack, downAttack; //hitboxes for directional attacks
+
+    bool attacking = false; //private bool for currently attacking
+    bool movedUp, movedDown, movedLeft, movedRight; //directional bools
+    Vector2 movement; //variable for not moving
+    Rigidbody2D rb; //player rigidbody
+    Collider2D col; //player hitbox
 
     // Start is called before the first frame update
     void Start()
     {
-        DisableAttacks();
+        DisableAttacks(); //disable the attack hitboxes
+        //get the component
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //move up if pressing up and not pressing down
-        if (Input.GetKey(KeyCode.W))
-        {
-            if (!Input.GetKey(KeyCode.S))
-            {
-                rb.transform.position =
-                    new Vector3
-                    (rb.transform.position.x,
-                    rb.transform.position.y + Time.deltaTime * speed,
-                    rb.transform.position.z);
-                movedUp = true;
-                movedLeft = movedRight = movedDown = false;
-            }
-        }
-        //move down if pressing down and not pressing up
-        else if (Input.GetKey(KeyCode.S))
-        {
-            rb.transform.position =
-                new Vector3
-                (rb.transform.position.x,
-                rb.transform.position.y - Time.deltaTime * speed,
-                rb.transform.position.z);
-            movedDown = true;
-            movedLeft = movedRight = movedUp = false;
-        }
-
-        //move left if pressing left and not right
-        if (Input.GetKey(KeyCode.A))
-        {
-            if (!Input.GetKey(KeyCode.D))
-            {
-                rb.transform.position =
-                    new Vector3
-                    (rb.transform.position.x - Time.deltaTime * speed,
-                    rb.transform.position.y,
-                    rb.transform.position.z);
-                movedLeft = true;
-                movedUp = movedDown = movedRight = false;
-            }
-        }
-        //move right if pressing right and not left
-        else if (Input.GetKey(KeyCode.D))
-        {
-            rb.transform.position =
-                new Vector3
-                (rb.transform.position.x + Time.deltaTime * speed,
-                rb.transform.position.y,
-                rb.transform.position.z);
-            movedRight = true;
-            movedUp = movedDown = movedLeft = false;
-        }
-
+        Move();
 
         if (Input.GetKey(KeyCode.Mouse0)) Attack();
         else if (attacking) DisableAttacks();
-
-
-
-
-
     }
 
     //set the hitboxes invisible
@@ -107,5 +54,16 @@ public class PlayerController : MonoBehaviour
         if (movedDown) downAttack.gameObject.SetActive(true);
         if (movedRight) rightAttack.gameObject.SetActive(true);
         attacking = true;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+    void Move()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
     }
 }
