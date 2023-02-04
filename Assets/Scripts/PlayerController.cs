2fb53f485a38c512ef9914fa7675caab41c1bb00
Ethan Johnson <ethan.d.r.johnson@gmail.com>
2023-02-03 21:31:39 -0500

    Read in and display attacks
    Determine attacks from direction last moved when attack was pressed
    When the player attacks display the attack hit area

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -9,11 +10,16 @@ public class PlayerController : MonoBehaviour
     public int speed; //player movement speed
     public Rigidbody2D rb; //player rigidbody
     public Collider2D col; //player hitbox
+    private bool movedUp, movedDown, movedLeft, movedRight; //directional bools
+    public Collider2D leftAttack, rightAttack, upAttack, downAttack;
+    internal bool attacking = false;
+    public float attackHideDelay;
+    internal float lastAttackTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        DisableAttacks();
     }
 
     // Update is called once per frame
@@ -29,6 +35,8 @@ public class PlayerController : MonoBehaviour
                     (rb.transform.position.x,
                     rb.transform.position.y + Time.deltaTime * speed,
                     rb.transform.position.z);
+                movedUp = true;
+                movedLeft = movedRight = movedDown = false;
             }
         }
         //move down if pressing down and not pressing up
@@ -39,6 +47,8 @@ public class PlayerController : MonoBehaviour
                 (rb.transform.position.x,
                 rb.transform.position.y - Time.deltaTime * speed,
                 rb.transform.position.z);
+            movedDown = true;
+            movedLeft = movedRight = movedUp = false;
         }
 
         //move left if pressing left and not right
@@ -51,6 +61,8 @@ public class PlayerController : MonoBehaviour
                     (rb.transform.position.x - Time.deltaTime * speed,
                     rb.transform.position.y,
                     rb.transform.position.z);
+                movedLeft = true;
+                movedUp = movedDown = movedRight = false;
             }
         }
         //move right if pressing right and not left
@@ -61,7 +73,39 @@ public class PlayerController : MonoBehaviour
                 (rb.transform.position.x + Time.deltaTime * speed,
                 rb.transform.position.y,
                 rb.transform.position.z);
+            movedRight = true;
+            movedUp = movedDown = movedLeft = false;
         }
 
+
+        if (Input.GetKey(KeyCode.Mouse0)) Attack();
+        else if (attacking) DisableAttacks();
+
+
+
+
+
+    }
+
+    //set the hitboxes invisible
+    private void DisableAttacks(string ignore = "")
+    {
+        Debug.Log("Disabled objects");
+        if (!ignore.Equals("left")) leftAttack.gameObject.SetActive(false);
+        if (!ignore.Equals("right")) rightAttack.gameObject.SetActive(false);
+        if (!ignore.Equals("up")) upAttack.gameObject.SetActive(false);
+        if (!ignore.Equals("down")) downAttack.gameObject.SetActive(false);
+        attacking = false;
+    }
+
+    private void Attack()
+    {
+        if (attacking) return;
+        Debug.Log("Attacked");
+        if (movedLeft) leftAttack.gameObject.SetActive(true);
+        if (movedUp) upAttack.gameObject.SetActive(true);
+        if (movedDown) downAttack.gameObject.SetActive(true);
+        if (movedRight) rightAttack.gameObject.SetActive(true);
+        attacking = true;
     }
 }
