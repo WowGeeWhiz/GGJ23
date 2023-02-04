6b15273a1edba173be1eb295d121b97b28de55f5
Ethan Johnson <ethan.d.r.johnson@gmail.com>
2023-02-04 17:18:44 -0500

    Lock controls on lose
    When the house health hits 0 lock all player controls

--- Assets/Scripts/HouseController.cs
+++ Assets/Scripts/HouseController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class HouseController : MonoBehaviour
 {
     public float maxHealth, currentHealth;
-    private PlayerController player;
+    public PlayerController player;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +16,7 @@ public class HouseController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentHealth <= 0) player.lockMovement = true;
     }
 
     public void changeDurability(float damage)
