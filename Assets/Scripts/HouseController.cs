d1f1b45ac442f2a51823aff14b3eac094bf4060e
DonovanLott <93689796+DonovanLott@users.noreply.github.com>
2023-02-05 11:32:22 -0500

    House Sound
    house sound

--- Assets/Scripts/HouseController.cs
+++ Assets/Scripts/HouseController.cs
@@ -9,12 +9,29 @@ public class HouseController : MonoBehaviour
     public float healthAmt, healCostInWood;
     private int repeatsLeft;
     public int repeatsPerCost;
+    float lastCheckedHealth;
+
+    float audioTimer, audioDelay, audioRate;
+    float audioTimer2, audioDelay2, audioRate2;
+    private AudioSource audioSource;
+    public AudioClip[] sounds;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
         repeatsLeft = repeatsPerCost;
+        lastCheckedHealth = currentHealth;
+
+        audioSource = GetComponent<AudioSource>();
+
+        audioTimer = 0;
+        audioRate = 4f;
+        audioDelay = 1 / audioRate;
+
+        audioTimer2 = 0;
+        audioRate2 = 2f;
+        audioDelay2 = 1 / audioRate2;
     }
 
     // Update is called once per frame
@@ -23,16 +40,41 @@ public class HouseController : MonoBehaviour
         if (currentHealth <= 0) player.lockMovement = true;
     }
 
+    void FixedUpdate()
+    {
+        audioTimer -= Time.deltaTime;
+
+        if (lastCheckedHealth > currentHealth)
+        {
+            if (audioTimer <= 0)
+            {
+                audioTimer = audioDelay;
+                PlayAudio(0, 5);
+            }
+        }
+        else if(lastCheckedHealth < currentHealth)
+        {
+            if (audioTimer2 <= 0)
+            {
+                audioTimer2 = audioDelay2;
+                PlayAudio(6, 6);
+            }
+        }
+        lastCheckedHealth = currentHealth;
+    }
+
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
         if (currentHealth > maxHealth) currentHealth = maxHealth;
+
     }
 
     public void HealHouse()
     {
         currentHealth += healthAmt;
         if (currentHealth > maxHealth) currentHealth = maxHealth;
+        
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -68,5 +110,12 @@ public class HouseController : MonoBehaviour
                 repeatsLeft--;
             }
         }
+    }
+
+    public void PlayAudio(int startIndex, int endIndex)
+    {
+        int index = Random.Range(startIndex, endIndex);
+        audioSource.clip = sounds[index];
+        audioSource.PlayOneShot(audioSource.clip, 0.7f);
     }
 }
